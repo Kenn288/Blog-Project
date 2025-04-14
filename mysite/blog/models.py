cf91a0e9dbbc1c685ec6f9584cd6f97df9f44d4f
Kenn288 <kariukikennedy288@gmail.com>
2025-04-14 19:43:54 +0300

    Updates on the Models file

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,8 @@
 from django.conf import settings
-from django.db import models
+from django.db.models import models
 from django.utils import timezone
+from django.db.models.functions import Now
+
 
 
 
